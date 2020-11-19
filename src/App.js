f8ba0df92a3bc810a1ac96d18fdd13139ee497ee
MacBook Pro Anthony <macbookproanthony@macbook-pro-de-anthony.home>
2020-11-19 21:24:41 +0100

    create a TravelCard to select your trip and see price, duration and citys

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Router from "./components/Router";
+import TravelCard from "./components/TravelCard";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router />
+      <TravelCard />
     </div>
   );
 }
