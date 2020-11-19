b4362e1a04e8c267e319a352e22d157c55177365
renaudfournet <fournet.renaud@gmail.com>
2020-11-19 19:19:18 +0100

    added responsive navbar for mobile

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -1,4 +1,5 @@
 import carriage from "./carriage.png";
 import user from "./man-with-tie-profile.png";
+import logo from "./main-logo.png";
 
-export { carriage, user };
+export { carriage, user, logo };
