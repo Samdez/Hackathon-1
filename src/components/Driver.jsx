c84739e9624f920d2a8899cf051e98e3f6aa7c98
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-20 09:04:26 +0100

    adjusted font on home

--- src/components/Driver.jsx
+++ src/components/Driver.jsx
@@ -11,16 +11,16 @@ const DriverComponent = () => {
         <img className="driverImg" src={drivercomponent1} alt="driverimg" />
       </div>
       <div className="containerText">
-        <h2 className="driverText">
+        <Typography variant='h2' className="driverText">
           You rather drive and share your carillage with new people ? Tel us
           where are you going !
-        </h2>
-        <p className="driverText">
+        </Typography>
+        <Typography variant='p' className="driverText">
           Together, lets help millions of people to travel easier and make this
           your least expensive journey ever. Every week, every month. To meet
           with a loved one, or to discover a new place. With a big family, or a
           big luggage. To Paris, Amsterdam or any other European destination.
-        </p>
+        </Typography>
         <button className="driverButton">Offer a ride</button>
       </div>
     </div>
